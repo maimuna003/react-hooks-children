d4ec95e52bf31da7cb94c88853bcc16b25728816
maimuna <mohamudmaimuna003@gmail.com>
2023-07-20 20:14:14 +0300

    completed assignment

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,38 @@
-test("Test passing", () => {
-  return new Promise((resolve, reject) => {
-    resolve(true);
-  });
-});
+import React from "react";
+import Container from "./Container";
+
+function App() {
+  return (
+    <div className="App">
+      <Container header="Example header" direction="horizontal">
+        <div>This is an example!</div>
+        <div>This is an example!</div>
+        <div>This is an example!</div>
+        <div>This is an example!</div>
+        <div>This is an example!</div>
+        <div>This is an example!</div>
+        <div>This is an example!</div>
+      </Container>
+      <Container header="Example header" textPosition="center">
+        <img
+          alt="turtle"
+          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
+        />
+        <img
+          alt="turtle"
+          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
+        />
+        <img
+          alt="turtle"
+          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
+        />
+        <img
+          alt="turtle"
+          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
+        />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
